9041e51ba085d1583180b822d78f5f9dada08547
alexandre-zeni <alexandre.zeni@philips.com>
2023-10-20 17:53:54 -0300

    adding start menu screen and game state handler

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ pygame.init()
 
 game_icon = pygame.image.load(r'tic_tac_toe_icon.png')
 board_lines_color = pygame.Color('black')
+game_state_handler = {
+    'start_menu': {
+        'state': True
+    },
+    'running': {
+        'state': False
+    },
+    'ended': {
+        'state': False
+    }
+}
 
 pygame.display.set_caption('tic-tac-toe-ai')
 pygame.display.set_icon(game_icon)
@@ -51,13 +62,27 @@ grid_quadrants = {
     },
 }
 
-while True:
-    board_helper.draw_board(pygame, display_surface, display_surface_width, display_surface_height, board_lines_color)
-    board_helper.handle_player_action(pygame, display_surface, grid_quadrants, board_lines_color)
+def handle_drawing(pygame, display_surface, display_surface_width, display_surface_height, board_lines_color):
+    if (game_state_handler['start_menu']['state'] == True):
+        draw_start_menu(pygame, display_surface, board_lines_color)
+    elif (game_state_handler['running']['state'] == True):
+        board_helper.draw_board(pygame, display_surface, display_surface_width, display_surface_height, board_lines_color)
 
+def draw_start_menu(pygame, display_surface, board_lines_color):
+    display_surface.blit(pygame.font.SysFont('comicsans', 35).render('Tic-Tac-Toe', 1, board_lines_color), (150, 220))
+    display_surface.blit(pygame.font.SysFont('comicsans', 20).render('Press space to initialize.', 1, board_lines_color), (150, 280))
+
+while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if (event.type == pygame.QUIT):
             pygame.quit()
             quit()
+        elif (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and game_state_handler['start_menu']['state'] == True):
+            game_state_handler['start_menu']['state'] = False
+            game_state_handler['running']['state'] = True
+        elif (event.type == pygame.MOUSEBUTTONDOWN and game_state_handler['running']['state'] == True):
+            board_helper.handle_player_action(pygame, display_surface, grid_quadrants, board_lines_color)
 
         pygame.display.update()
+        display_surface.fill(pygame.Color('white'))
+        handle_drawing(pygame, display_surface, display_surface_width, display_surface_height, board_lines_color)
